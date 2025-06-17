3c12bbd8c547cc9e1bff4f52fb4bc7023168e445
Joe George <joe@zeroc.com>
2025-06-17 13:38:17 -0400

    fixes

--- matlab/toolbox/generateApiReference.m
+++ matlab/toolbox/generateApiReference.m
@@ -1,25 +1,36 @@
+% Script:
+% Generate Ice for MATLAB documentation using m2docgen
 
-cd(fileparts(which(mfilename)));
+% cd(fileparts(which(mfilename)));
 cPath = fileparts(which(mfilename));
 
 mF = string(fileparts(cPath));
 src = fullfile(mF, 'src');
-oF = fullfile(mF,"m2docgen_documentation");
+oF = fullfile(mF, "m2docgen_documentation");
 
-opts = struct(  'toolboxName',      "Ice",
-                'delOld',           true, 
-                'mFolder',          [src], 
-                'outputFolder',     [oF], 
-                'excludeFolder',	["toolbox"],
-                'excludeFile',      [""],
-                'htmlMetaFolder',   "ressources",
-                'htmlTemplate',     "m2doc-standard",
-                'startPage',        ["Welcome_to_m2docgen.html"],
-                'toc',              [],
-                'verbose',          true);
-             
+opts = struct(
+    'toolboxName',
+    "Ice",
+    'delOld',
+    true,
+    'mFolder',
+    [src],
+    'outputFolder',
+    [oF],
+    'excludeFolder',
+    ["toolbox"],
+    'excludeFile',
+    [""],
+    'htmlMetaFolder',
+    "ressources",
+    'htmlTemplate',
+    "m2doc-standard",
+    'startPage',
+    ["Welcome_to_m2docgen.html"],
+    'toc',
+    [],
+    'verbose',
+    true);
 
-% make sure to have added m2docgen to the matlab path
-res = m2docgen(opts);
-doc
-% if the building of the search database fails, run the script again!
+% make sure to have added m2docgen to the matlab path res = m2docgen(opts);
+doc % if the building of the search database fails, run the script again !
