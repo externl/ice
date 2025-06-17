6f7422afc38fccd5757d47e601244dd3722cd670
Joe George <joe@zeroc.com>
2025-06-17 13:43:58 -0400

    fixes

--- matlab/toolbox/generateApiReference.m
+++ matlab/toolbox/generateApiReference.m
@@ -6,31 +6,21 @@ cPath = fileparts(which(mfilename));
 
 mF = string(fileparts(cPath));
 src = fullfile(mF, 'src');
-oF = fullfile(mF, "m2docgen_documentation");
+oF = fullfile(mF,"m2docgen_documentation");
 
-opts = struct(
-    'toolboxName',
-    "Ice",
-    'delOld',
-    true,
-    'mFolder',
-    [src],
-    'outputFolder',
-    [oF],
-    'excludeFolder',
-    ["toolbox"],
-    'excludeFile',
-    [""],
-    'htmlMetaFolder',
-    "ressources",
-    'htmlTemplate',
-    "m2doc-standard",
-    'startPage',
-    ["Welcome_to_m2docgen.html"],
-    'toc',
-    [],
-    'verbose',
-    true);
+opts = struct(  'toolboxName',      "Ice", 
+                'delOld',           true, 
+                'mFolder',          src, 
+                'outputFolder',     oF, 
+                'excludeFolder',    "toolbox",
+                'excludeFile',      string.empty,
+                'htmlMetaFolder',   "ressources",  % check spelling!
+                'htmlTemplate',     "m2doc-standard",
+                'startPage',        "Welcome_to_m2docgen.html",
+                'toc',              string.empty, 
+                'verbose',          true);
 
-% make sure to have added m2docgen to the matlab path res = m2docgen(opts);
-doc % if the building of the search database fails, run the script again !
+% make sure to have added m2docgen to the matlab path
+res = m2docgen(opts);
+
+% if the building of the search database fails, run the script again!
