0e1728b20921301a552f420995fdb3ad0f007f6f
Joe George <joe@zeroc.com>
2025-06-17 15:16:01 -0400

    revert

--- matlab/toolbox/generateApiReference.m
+++ matlab/toolbox/generateApiReference.m
@@ -1,7 +1,22 @@
-disp("MATLAB is running in CI");
+% Script:
+% Generate Ice for MATLAB documentation using m2docgen
 
-a = 1;
-b = 2;
-c = a + b;
+% cd(fileparts(which(mfilename)));
+cPath = fileparts(which(mfilename));
+mF = fileparts(cPath);
+src = fullfile(mF, 'src');
+oF = fullfile(mF, 'm2docgen_documentation');  % use char for safety
 
-disp("Sum is: " + c);
+opts = struct(  'toolboxName',      'Ice',
+                'delOld',           true,
+                'mFolder',          src,
+                'outputFolder',     oF,
+                'excludeFolder',    'toolbox',
+                'excludeFile',      {''},
+                'htmlMetaFolder',   'resources',
+                'htmlTemplate',     'm2doc-standard',
+                'startPage',        'Welcome_to_m2docgen.html',
+                'toc',              {''},
+                'verbose',          true);
+
+res = m2docgen(opts);
